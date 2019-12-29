99a35befca698edf9b54cb523ea528943a505691
minhsieh-ubuntu <side112358@gmail.com>
2019-12-29 18:20:45 +0800

    House Cleaning 2019-12-29

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,29 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+// $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+//     // Sample log message
+//     $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+//     // Render index view
+//     return $this->renderer->render($response, 'index.phtml', $args);
+// });
+
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+	return $this->renderer->render($response, 'index.phtml', $args);	
+});
+
+$app->get('/md5', function (Request $request, Response $response, array $args) {
+	return $this->renderer->render($response, 'md5.phtml' , $args);
+});
+
+$app->post('/md5', function (Request $request, Response $response, array $args) {
+	$body = $request->getParsedBody();
+	$strings = explode("\n",$body['string']);
+	$md5 = [];
+	foreach($strings as $one){
+		$md5[] = md5($one);
+	}
+	return $response->withJson(['md5' => $md5 ]);
 });
